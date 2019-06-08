0eed64c76fad18cbfc58c8756c8d7c13ada30687
Rohit Musti <rohitmusti@Rohits-MacBook-Pro.local>
2019-06-07 23:02:34 -0400

    beginning the process of building the whole app, starting by removing some default react stuff

--- client/src/App.js
+++ client/src/App.js
@@ -22,18 +22,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p class="App-intro">
             {this.state.apiTestResp}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
